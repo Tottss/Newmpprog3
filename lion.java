1416d58422cdd0e05241ac7cc4f13d7c7d25915a
tylerCo104 <160320598+tylerCo104@users.noreply.github.com>
2025-03-09 19:45:30 +0800

    Update lion.java

--- lion.java
+++ lion.java
@@ -6,6 +6,17 @@ public class lion extends Piece {
         super('L', 7);
     }
 
+    public void crossLake (char m) {
+	if (m == 'W')
+		this.setPosition(this.getRow() - 3, this.getColumn());
+	else if (m == 'S')
+		this.setPosition(this.getRow() + 3, this.getColumn());
+	else if (m == 'A')
+		this.setPosition(this.getRow(), this.getColumn() - 4);
+	else if (m == 'D')
+		this.setPosition(this.getRow(), this.getColumn() + 4);
+    }
+	
     // checks if next grid is crossable
     @Override
     public boolean canCross(grid nextSpace) {
