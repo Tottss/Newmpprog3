0d956bc3d72e3ceec0d1a06fafd3d5d301020200
Tottss <jeru_fabricante@dlsu.edu.ph>
2025-04-01 15:56:06 +0800

    WORKING FINAL
    GUI CHANGES NALANG

--- AppFrame.java
+++ AppFrame.java
@@ -13,7 +13,7 @@ public class AppFrame extends JFrame {
 
     public AppFrame(){
         setTitle("Jungle King");
-		setSize(1000, 800);
+		setSize(1100, 735);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
